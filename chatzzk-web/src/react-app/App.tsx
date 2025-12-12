41bb1a34beb2f259de944b5c98f92ee869730696
mrsuit0114 <woosa0114@gmail.com>
2025-12-13 01:07:20 +0900

    feat: init chatzzk-web with hono, react, vite

---
+++ chatzzk-web/src/react-app/App.tsx
@@ -0,0 +1,44 @@
+// src/App.tsx
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+	const [name, setName] = useState("unknown");
+
+	return (
+		<>
+			<h1>Vite + React + Hono + Cloudflare</h1>
+			<div className="card">
+				<button
+					onClick={() => setCount((count) => count + 1)}
+					aria-label="increment"
+				>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<div className="card">
+				<button
+					onClick={() => {
+						fetch("/api/")
+							.then((res) => res.json() as Promise<{ name: string }>)
+							.then((data) => setName(data.name));
+					}}
+					aria-label="get name"
+				>
+					Name from API is: {name}
+				</button>
+				<p>
+					Edit <code>worker/index.ts</code> to change the name
+				</p>
+			</div>
+			<p className="read-the-docs">Click on the logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
---
+++ chatzzk-web/src/worker/index.ts
@@ -0,0 +1,6 @@
+import { Hono } from "hono";
+const app = new Hono<{ Bindings: Env }>();
+
+app.get("/api/", (c) => c.json({ name: "Cloudaaaaaflare" }));
+
+export default app;
